25239d2e9c4054210650acf487104fb030f64f12
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-03-11 06:36:45 +0300

    add addEventListener mouseenter and mouseleave

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const card = document.querySelector('.card')
 const container = document.querySelector('.container')
 
-container.addEventListener('mousemove', (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 10
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 10
+const title = document.querySelector('.title')
+
+container.addEventListener('mousemove', e => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+})
+
+//Animate In
+container.addEventListener('mouseenter', e => {
+  card.style.transition = 'none'
+  //Popout
+  title.style.transform = 'translateZ(150px)'
+})
+
+//Anmate Out
+container.addEventListener('mouseleave', e => {
+  card.style.transition = 'all 0.5s ease'
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`
+  //Popback
+  title.style.transform = 'translateZ(0px)'
 })
