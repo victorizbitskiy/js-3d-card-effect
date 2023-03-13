711d2395737be8c488ab30551c42160ff25d806e
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-03-13 12:00:59 +0300

    add 3d effect to other elmts

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const card = document.querySelector('.card')
 const container = document.querySelector('.container')
 
 const title = document.querySelector('.title')
-const sneaker = document.querySelector('.sneaker')
+const sneaker = document.querySelector('.sneaker img')
+const description = document.querySelector('.info h3')
+const sizes = document.querySelector('.sizes')
+const purchase = document.querySelector('.purchase button')
 
 container.addEventListener('mousemove', e => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 25
@@ -15,6 +18,10 @@ container.addEventListener('mouseenter', e => {
   card.style.transition = 'none'
   //Popout
   title.style.transform = 'translateZ(150px)'
+  sneaker.style.transform = 'translateZ(200px) rotateZ(-45deg)'
+  description.style.transform = 'translateZ(125px)'
+  sizes.style.transform = 'translateZ(100px)'
+  purchase.style.transform = 'translateZ(75px)'
 })
 
 //Anmate Out
@@ -23,4 +30,8 @@ container.addEventListener('mouseleave', e => {
   card.style.transform = `rotateY(0deg) rotateX(0deg)`
   //Popback
   title.style.transform = 'translateZ(0px)'
+  sneaker.style.transform = 'translateZ(0) rotateZ(0deg)'
+  description.style.transform = 'translateZ(0)'
+  sizes.style.transform = 'translateZ(0)'
+  purchase.style.transform = 'translateZ(0)'
 })
